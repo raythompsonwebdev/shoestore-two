1fd3a398d92dd8f61e7bdd2fc8c279a8e425b037
raythompsonwebdev <raymond.thompson@raythompsonwebdev.co.uk>
2024-12-31 15:30:25 +0000

    update package json - replaced css @import with @use - next.config changed from cjs to mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
 const nextConfig = {
   reactStrictMode: true,
@@ -17,4 +20,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
